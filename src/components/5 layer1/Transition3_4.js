0d021c0beb981b8cb47f1f931355e4bbfc21ed2d
DippyArtu <artur.makhnach@gmail.com>
2023-03-05 17:27:58 +0400

    layer1 wip, transition3-4 + cosmetics

--- src/components/5 layer1/Transition3_4.js
+++ src/components/5 layer1/Transition3_4.js
@@ -1,10 +1,16 @@
 import React from 'react';
+
+import '../../styles/5 layer1/Transition3_4.css'
 import transitionImage34 from '../../images/3-overlay-grad.png'
+import transitionNoise34 from '../../images/3-overlay-noise.png'
 
 function Transition34() {
     return (
       <div>
         <img src={transitionImage34} alt="transition-image3-4" className="transition-image3-4" />
+        <img src={transitionNoise34} alt="transition-noise3-4" className="transition-noise3-4" />
+        <p className="limits-text3-4">without limits</p>
+        <h2 className="chatting-text3-4">Simply start chatting<br/><br/>Ask questions, engage in discussions, generate images, retrieve information from the web, create code and infinitely more</h2>
       </div>
     );
   }
