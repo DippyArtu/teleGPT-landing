f61bcbb5bb8d0ebf703ecaa90fc84038fd56ae08
DippyArtu <artur.makhnach@gmail.com>
2023-03-06 05:08:17 +0400

    scaling and overflow fix wip

--- src/components/5 layer1/6 elements/BotImage6.js
+++ src/components/5 layer1/6 elements/BotImage6.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../../../styles/5 layer1/BotImage6.css';
 
+import '../../../styles/5 layer1/BotImage6.css';
 import botImage6 from '../../../images/6-bot.svg';
 
 const BotImage6 = () => {
--- src/components/5 layer1/6 elements/Elements6.js
+++ src/components/5 layer1/6 elements/Elements6.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import BotImage6 from './BotImage6';
 
+import '../../../styles/5 layer1/Elements6.css';
 import gradImage6 from '../../../images/6-grad.png';
 import noiseImage6 from '../../../images/6-noise.png';
-
-import '../../../styles/5 layer1/Elements6.css';
+import text6 from '../../../images/6-text.svg'
 
 function Elements6() {
     return (
     <div>
-        {/* redu this as an image */}
-        {/* <div className="neural-networks">
-            <p className="neural-networks-text">
-                Neural networks are complex systems that are modeled after the human brain, capable of learning from data and making predictions. They work by passing information through multiple layers of interconnected nodes, where each node performs a simple mathematical operation. By iteratively adjusting the strength of connections between nodes, neural networks can discover and capture patterns in the data, allowing them to make accurate predictions and decisions.
-            </p>
-        </div> */}
-        {/* <img src={gradImage6} alt="gradient6" className="gradient6" /> */}
-        {/* <img src={noiseImage6} alt="noise6" className="noise6" /> */}
-        {/* <BotImage6 /> */}
-        {/* <div className="powered-by">
+        <img src={text6} alt="text6" className="text6" />
+        <img src={gradImage6} alt="gradient6" className="gradient6" />
+        <img src={noiseImage6} alt="noise6" className="noise6" />
+        <BotImage6 />
+        <div className="powered-by">
             <span>powered by </span>
             <a href="https://defimoon.org" target="_blank" rel="noopener noreferrer"><b>defimoon</b></a>
-        </div> */}
-        {/* <div className="welcome-container">
+        </div>
+        <div className="welcome-container">
             <h1 className="welcome-text">Welcome</h1>
-        </div> */}
+        </div>
     </div>
     );
   }
